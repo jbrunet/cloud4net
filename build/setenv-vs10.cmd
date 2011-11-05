fb149b915ac2ff4df0f2a9b1a00d501c1f6c1f0e
Julien Brunet <jbrunet@topian.net>
2011-11-05 16:00:12 +0100

    - source for 1.0.12 packages - 3.5 projects are back, now compiled from 2010 - upgraded to latest NuGet.exe

--- build/setenv-vs10.cmd
+++ build/setenv-vs10.cmd
@@ -1,4 +1,4 @@
-@SET PATH=%PATH%;%WinDir%\Microsoft.NET\Framework\v4.0.21006
+@SET PATH=%PATH%;%WinDir%\Microsoft.NET\Framework\v4.0.30319
 @SET VSVER=10.0
 @ECHO Cloud4Net  VS10 Command-line Build
 @ECHO Enter 'msbuild' to build the package (both 3.5 and 4.0 versions)
